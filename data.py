548a432b460f756a172b09143298bc0d629377a7
Zoe Qiwen <qiwenkolimntcode@gmail.com>
2026-05-12 14:42:32 +0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,40 @@
+data = [
+    {
+        "nama":"alya pertama",
+        "followers_count":677777,
+        "description":"lifestyle & daily vlogs",
+        "country": "indonesia",
+    },
+
+    {
+        "nama": "koko khanti",
+        "followers_count": 111111111277777777777777779997,
+        "description": "tech review and gadgets tips",
+        "country": "indonesia",
+    },
+
+    {
+        "nama": "koogjjj",
+        "followers_count": 61111,
+        "description": "food creator sharing quick recipes and street food hunts",
+        "country": "indonesia",
+    },
+    {
+        "nama": "qqqqhhhhhh",
+        "followers_count": 70908684950375,
+        "description": "fitness coah ",
+        "country": "indonesia",
+    },
+    {
+        "nama": "rkdnnj",
+        "followers_count": 99898,
+        "description": "coffe lover",
+        "country": "indonesia",
+    },
+    {
+        "nama": "ravi",
+        "followers_count": 77777,
+        "description": "simple aesthetic content",
+        "country": "indonesia",
+    },
+]
